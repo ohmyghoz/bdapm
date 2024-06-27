884798018d9424f7d9324db7d67e17f23e018490
reza <reza.andrian@ojk.go.id>
2024-06-27 18:39:40 +0700

    Update Database

--- ConsoleTestingHive/Program.cs
+++ ConsoleTestingHive/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 using System.Text.RegularExpressions;
 using System.Data.Odbc;
+using System.Data;
 
 namespace ConsoleQueue
 {
--- SyncWebService/Settings1.Designer.cs
+++ SyncWebService/Settings1.Designer.cs
@@ -12,7 +12,7 @@ namespace SyncWebService {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class Settings1 : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings1 defaultInstance = ((Settings1)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings1())));
@@ -62,8 +62,8 @@ namespace SyncWebService {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQL2019;Initial Catalog=BDA;Persist Security Info=True;User ID=sa;P" +
-            "assword=dodol132!")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=10.225.60.50;Initial Catalog=BDA2023;Persist Security Info=True;User " +
+            "ID=sa;Password=password.1")]
         public string ConnStringSQL {
             get {
                 return ((string)(this["ConnStringSQL"]));
