773569220401c6db95e4f95d052fcc958615a05b
Muhammad Ghozy Ul-Haq <ulhaq.ghozy@gmail.com>
2024-07-26 16:39:25 +0700

    Update Page MarketDriven

---
+++ BDA/Areas/BDAPM/Controllers/MarketDrivenController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Aspose.Cells;
+using BDA.DataModel;
+using DevExpress.DocumentServices.ServiceModel.DataContracts;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BDA.Controllers
+{
+    [Area("BDAPM")]
+    public class MarketDrivenController : Controller
+    {
+        private DataEntities db;
+        private IWebHostEnvironment _env;
+        public MarketDrivenController(DataEntities db, IWebHostEnvironment env)
+        {
+            this.db = db;
+            _env = env;
+        }
+            public IActionResult Index()
+        {
+                var mdl = new BDA.Models.MenuDbModels(db, Microsoft.AspNetCore.Http.Extensions.UriHelper.GetDisplayUrl(db.httpContext.Request).ToLower());
+                var currentNode = mdl.GetCurrentNode();
+                string pageTitle = currentNode != null ? currentNode.Title : ""; //menampilkan data menu
+
+                db.CheckPermission("Market Driven View", DataEntities.PermissionMessageType.ThrowInvalidOperationException); //check permission nya view/lihat nya
+                ViewBag.Export = db.CheckPermission("Market Driven Export", DataEntities.PermissionMessageType.NoMessage); //check permission export
+                db.InsertAuditTrail("MarketDriven_Akses_Page", "Akses Page Market Driven", pageTitle); //simpan kedalam audit trail
+
+                return View();
+        
+        }
+    }
+}
--- DataModelBDAPM/DataModel.Designer.cs
+++ DataModelBDAPM/DataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\DATA REZA\OJK\Source\2024\BDAPM\BDA-PM-2024\DataModelBDAPM\DataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Download\BDAmasReza\DataModelBDAPM\DataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
