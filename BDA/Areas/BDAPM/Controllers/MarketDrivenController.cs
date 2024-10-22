3e030f0ee9049db45818eb196d96d08915cdafa2
Muhammad Ghozy Ul-Haq <ulhaq.ghozy@gmail.com>
2024-10-23 00:51:17 +0700

    halaman validasi data transaksi pivotgrid

--- BDA/Areas/BDAPM/Controllers/MarketDrivenController.cs
+++ BDA/Areas/BDAPM/Controllers/MarketDrivenController.cs
@@ -32,66 +32,26 @@ namespace BDA.Controllers
             this.db = db;
             _env = env;
         }
-            public IActionResult Index()
+        public IActionResult Index()
         {
-                var mdl = new BDA.Models.MenuDbModels(db, Microsoft.AspNetCore.Http.Extensions.UriHelper.GetDisplayUrl(db.httpContext.Request).ToLower());
-                var currentNode = mdl.GetCurrentNode();
-                string pageTitle = currentNode != null ? currentNode.Title : ""; //menampilkan data menu
+            var mdl = new BDA.Models.MenuDbModels(db, Microsoft.AspNetCore.Http.Extensions.UriHelper.GetDisplayUrl(db.httpContext.Request).ToLower());
+            var currentNode = mdl.GetCurrentNode();
+            string pageTitle = currentNode != null ? currentNode.Title : ""; //menampilkan data menu
 
-                db.CheckPermission("Market Driven View", DataEntities.PermissionMessageType.ThrowInvalidOperationException); //check permission nya view/lihat nya
-                ViewBag.Export = db.CheckPermission("Market Driven Export", DataEntities.PermissionMessageType.NoMessage); //check permission export
-                db.InsertAuditTrail("MarketDriven_Akses_Page", "Akses Page Market Driven", pageTitle); //simpan kedalam audit trail
+            // Display the value of pageTitle
+            Console.WriteLine(pageTitle);
 
-                return View();
-        
+            db.CheckPermission("Market Driven View", DataEntities.PermissionMessageType.ThrowInvalidOperationException); //check permission nya view/lihat nya
+            ViewBag.Export = db.CheckPermission("Market Driven Export", DataEntities.PermissionMessageType.NoMessage); //check permission export
+            db.InsertAuditTrail("MarketDriven_Akses_Page", "Akses Page Market Driven", pageTitle); //simpan kedalam audit trail
+
+            return View();
         }
 
         public ActionResult AjaxRequest()
         {
             return View();
         }
-
-        public ActionResult Index2()
-        {
-            List<market_driven_rg_ng> marketData = new List<market_driven_rg_ng>();
-
-            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                string sql = "SELECT * FROM dbo.pasarmodal_market_driven_rg_ng";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            //marketData.Add(new MarketDrivenModel
-                            //{
-                            //    HistoryType = reader["history_type"].ToString(),
-                            //    PeriodeLvl1 = reader["periode_lvl1"].ToString(),
-                            //    PeriodeLvl2 = reader["periode_lvl2"].ToString(),
-                            //    PeriodeLvl3 = reader["periode_lvl3"].ToString(),
-                            //    SecurityCode = reader["security_code"].ToString(),
-                            //    Market = reader["market"].ToString(),
-                            //    Volume = Convert.ToInt32(reader["volume"]),
-                            //    Value = Convert.ToDecimal(reader["value"]),
-                            //    Freq = Convert.ToInt32(reader["freq"]),
-                            //    Low = Convert.ToDecimal(reader["low"]),
-                            //    High = Convert.ToDecimal(reader["high"]),
-                            //    Close = Convert.ToDecimal(reader["close"]),
-                            //    Periode = reader["periode"].ToString()
-                            //});
-                        }
-                    }
-                }
-            }
-
-            return View(marketData);
-        }
-
 
 
         public IActionResult LeaderVsLaggard()
@@ -144,7 +104,7 @@ namespace BDA.Controllers
 
             db.CheckPermission("Market Driven View", DataEntities.PermissionMessageType.ThrowInvalidOperationException); //check permission nya view/lihat nya
             ViewBag.Export = db.CheckPermission("Market Driven Export", DataEntities.PermissionMessageType.NoMessage); //check permission export
-            db.InsertAuditTrail("Validasi_Data_Transaksi", "Akses Page Validasi Data Transaksi", pageTitle); //simpan kedalam audit trail
+            db.InsertAuditTrail("ValidasiDataTransaksi_Akses_Page", "Akses Page Validasi Data Transaksi", pageTitle); //simpan kedalam audit trail
 
             return View();
 
@@ -176,6 +136,89 @@ namespace BDA.Controllers
             return View();
 
         }
+
+        public ActionResult SimpanPenggunaanData(string id)
+        {
+            string message = "";
+            string Penggunaan_Data = "";
+            bool result = true;
+            var userId = HttpContext.User.Identity.Name;
+
+            var mdl = new BDA.Models.MenuDbModels(db, Microsoft.AspNetCore.Http.Extensions.UriHelper.GetDisplayUrl(db.httpContext.Request).ToLower());
+            var currentNode = mdl.GetCurrentNode();
+            string pageTitle = currentNode != null ? currentNode.Title : "";
+            db.InsertAuditTrail("MarketDriven_Akses_Page", "user " + userId + " mengakses halaman Market Driven untuk digunakan sebagai " + Penggunaan_Data + "", pageTitle);
+
+            try
+            {
+                string strSQL = db.appSettings.DataConnString;
+                using (SqlConnection conn = new SqlConnection(strSQL))
+                {
+                    conn.Open();
+                    string strQuery = "Select * from MasterPenggunaanData where id=" + id + " order by id asc ";
+                    SqlDataAdapter da = new SqlDataAdapter(strQuery, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        Penggunaan_Data = dt.Rows[0]["Penggunaan_Data"].ToString();
+                    }
+                    conn.Close();
+                    conn.Dispose();
+                }
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = ex.Message;
+                message = "Saving Failed !, " + " " + errMsg;
+                result = false;
+            }
+            return Json(new { message, success = result }, new Newtonsoft.Json.JsonSerializerSettings());
+        }
+        public ActionResult SimpanPenggunaanDataVDT(string id)
+        {
+            string message = "";
+            string Penggunaan_Data = "";
+            bool result = true;
+            var userId = HttpContext.User.Identity.Name;
+
+            var mdl = new BDA.Models.MenuDbModels(db, Microsoft.AspNetCore.Http.Extensions.UriHelper.GetDisplayUrl(db.httpContext.Request).ToLower());
+            var currentNode = mdl.GetCurrentNode();
+            string pageTitle = currentNode != null ? currentNode.Title : "";
+
+            
+            db.InsertAuditTrail("ValidasiDataTransaksi_Akses_Page", "user " + userId + " mengakses halaman Validasi Data Transaksi untuk digunakan sebagai " + Penggunaan_Data + "", pageTitle);
+
+            try
+            {
+                string strSQL = db.appSettings.DataConnString;
+                using (SqlConnection conn = new SqlConnection(strSQL))
+                {
+                    conn.Open();
+                    string strQuery = "Select * from MasterPenggunaanData where id=" + id + " order by id asc ";
+                    SqlDataAdapter da = new SqlDataAdapter(strQuery, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        Penggunaan_Data = dt.Rows[0]["Penggunaan_Data"].ToString();
+                    }
+                    conn.Close();
+                    conn.Dispose();
+                }
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = ex.Message;
+                message = "Saving Failed !, " + " " + errMsg;
+                result = false;
+            }
+            return Json(new { message, success = result }, new Newtonsoft.Json.JsonSerializerSettings());
+        }
+
+
 
     }
 }
